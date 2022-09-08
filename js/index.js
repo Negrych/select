ab2c34c10c43b3b85f8892b81569d56418be74ab
Mykola Negrych <nehrych@outlook.com>
2022-09-08 23:36:51 +0300

    tab keyup

--- js/index.js
+++ js/index.js
@@ -5,43 +5,62 @@ const svgUp = document.querySelector(".up");
 const svgDown = document.querySelector(".down");
 
 selectTitle.value=selectItems[0].innerHTML;
-selectItems[0].classList.add("active")
-selectTitle.style.fontWeight="600"
-selectList.style.display="none"
+selectItems[0].classList.add("active");
+selectTitle.style.fontWeight="600";
+selectList.style.display="none";
 svgUp.style.display="none";
 
 selectItems.forEach(elem=>{
-    elem.addEventListener("click",click)
+    elem.addEventListener("click",click);
+    elem.addEventListener("keyup",(e)=>{
+        if (e.code === "Enter") {
+            click(e)
+        }
+    });
+    elem.focus();
 })
 
 window.addEventListener("click",(e)=>{
     if(e.target.classList.value !== "select-title") {
-        closeSelect()
+        closeSelect();
     }else {
-       openSelect()
+       openSelect();
+        e.target.focus();
+    }
+})
+
+window.addEventListener("keyup",(e)=>{
+    if (e.code === "Enter") {
+        if(e.target.classList.value !== "select-title") {
+            closeSelect();
+        }else {
+            openSelect();
+            e.target.focus();
+        }
     }
 })
 
 function click(e) {
     selectItems.forEach(elem=>{
-        elem.classList.remove("active")
+        elem.classList.remove("active");
     })
     e.target.classList.add("active");
     selectTitle.value=e.target.innerHTML;
+    e.target.focus();
 
-    closeSelect()
+    closeSelect();
 }
 
 function openSelect() {
-    selectTitle.style.display="none"
-    selectList.style.display="block"
+    selectTitle.style.display="none";
+    selectList.style.display="block";
     svgUp.style.display="block";
     svgDown.style.display="none";
 }
 
 function closeSelect() {
-    selectTitle.style.display="block"
-    selectList.style.display="none"
+    selectTitle.style.display="block";
+    selectList.style.display="none";
     svgUp.style.display="none";
     svgDown.style.display="block";
 }
